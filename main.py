ce6175b482d7f3c77db052b787fa902801addaf1
rafaelmpereira <51132459+rafaelmpereira@users.noreply.github.com>
2020-04-16 13:50:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,14 @@ from umqtt.simple import MQTTClient
 from machine import Pin
 import network
 import time
-"""
-import ds18x20
-import onewire
-
-# the device is on GPIO12
-dat =Pin(12)
-# create the onewire object
-ds = ds18x20.DS18X20(onewire.OneWire(dat))
-# scan for devices on the bus
-roms = ds.scan()
-print('found devices:', roms)
-"""
 
 #wifi setting
 ssid="NET_2GEF16EA"             #insert your wifi ssid
 password=""                     #insert your wifi password
-
 server = "mqtt.mydevices.com"
-clientid = "" #insert your client ID
-username = "" #insert your MQTT username
-password = "" #insert your MQTT password
+clientid = "" 			#insert your client ID
+username = "" 			#insert your MQTT username
+password = "" 			#insert your MQTT password
 
 topic = ("v1/%s/things/%s/" % (username, clientid))
 
@@ -49,8 +36,7 @@ def senddata():
   value = 25.4
   channel = "1"
   data = ("%s,%s=%s/%s" %(type, unit, value, channel))
-  
-  
+    
   """
   # forma de envio
   def getDataTopic(self, channel):
