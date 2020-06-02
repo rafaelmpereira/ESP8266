9cb08698ea9c7d3f34d7fe9b5affebdca6b85946
rafaelmpereira <51132459+rafaelmpereira@users.noreply.github.com>
2020-06-01 21:58:55 -0300

    Create testedht.py

---
+++ testedht.py
@@ -0,0 +1,28 @@
+
+
+
+
+import dht
+from machine import Pin
+from time import sleep
+
+
+d = dht.DHT11(Pin(13))
+led = Pin(2, Pin.OUT)
+led.value(1) # NO ESP12E (RAFAEL) O LED FICA DESLIGADO EM SINAL ALTO
+
+def readDHT11():
+  d.measure()
+  led.value(not led.value())
+  sleep(0.2)
+  led.value(not led.value())
+  dt = d.temperature()
+  print("Temperatura: "+ str(dt))
+  return str(dt)
+  sleep(1)
+
+readDHT11()
+
+
+
+
